ad6d51ac01f57ac1ccc4f578d0f4e84d16a77d7e
sritamkumar123 <sritamkumarrout@gmail.com>
2024-08-03 19:53:04 +0530

    route changes

--- src/test/java/api/endPOINTS/Routes.java
+++ src/test/java/api/endPOINTS/Routes.java
@@ -2,4 +2,16 @@ package api.endPOINTS;
 
 public class Routes {
 
+	public static String baseURL = "https://petstore.swagger.io/v2";
+	
+	// user module URLs 
+	 public static String post_url = baseURL+"/user";
+	 public static String get_url= baseURL+ "/user/{username}";
+	 public static String update_url = baseURL+"/user/{username}";
+	 public static String delete_url = baseURL +"/user/{username}";
+	 
+	 
+	 
+	
+	
 }
